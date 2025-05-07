874db9c14d1330bb94916f0f800b7faa980398fd
Muhammad Rasikh Riaz <mo.rasikhriaz@gmail.com>
2025-05-07 16:10:17 +0000

    Create bucket with versioning enabled

--- s3/main.tf
+++ s3/main.tf
@@ -1,18 +1,31 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "rasikh-bucket2322"
-  tags = {
-    Name = "mys3bucket"
-}
-}
-
-
+#resource "aws_s3_bucket" "mybucket" {
+#  bucket = "rasikh-bucket2322"
+#  tags = {
+#    Name = "mys3bucket"
+#}
+#}
+#
+#
 #Store object in a s3 bucket
+#
+#resource "aws_s3_object" "object" {
+#  bucket = aws_s3_bucket.mybucket.id
+#  key = "rasikh/rasikh.txt"
+#  source = "./rasikh.txt"
+#
+#
+#  etag = filemd5("./rasikh.txt")
+#}
 
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.mybucket.id
-  key = "rasikh/rasikh.txt"
-  source = "./rasikh.txt"
 
+# Create a general purpose bucket with versioning enabled.
 
-  etag = filemd5("./rasikh.txt")
+resource "aws_s3_bucket" "example" {
+   bucket = "63535525-bucket"   #name must be unique
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+     status = "Enabled"
+  }
 }
